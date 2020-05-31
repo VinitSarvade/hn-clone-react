bddfb48c25f3a7becd9b164c2ee1c69cb9827fe5
Vinit Sarvade <vinit.sarvade.08@gmail.com>
2020-05-31 23:11:38 +0530

    Run tests on circleci

--- src/containers/app/app.test.js
+++ src/containers/app/app.test.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./index";
+import { BrowserRouter } from "react-router-dom";
 
 test("renders the Hacker News navbar", () => {
-  const { getByText } = render(<App />);
-  const title = getByText(/Hacker News/i);
+  const { getByText } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const title = getByText(/acker News/i);
   expect(title).toBeInTheDocument();
 });
 
 test("renders the news table headings", () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   const commentsHeading = getByText(/Comments/i);
   const votesHeading = getByText(/Votes Count/i);
   const upvoteHeading = getByText(/Upvote/i);

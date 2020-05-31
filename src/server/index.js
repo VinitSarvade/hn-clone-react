e320c438ab24345076ffe4ef1eeb21032fa3e3a2
Vinit Sarvade <vinit.sarvade.08@gmail.com>
2020-05-31 22:47:09 +0530

    Fixing plugin-transform-runtime error

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ require("ignore-styles");
 require("@babel/register")({
   presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [
-    //"@babel/plugin-transform-modules-commonjs",
+    "@babel/plugin-transform-runtime",
     [
       "transform-assets",
       {
